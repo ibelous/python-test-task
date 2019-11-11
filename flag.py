6a47fb80c5fe78d7c5c99767b0f3a2745925f650
Igor <igrek145@gmail.com>
2019-11-11 12:13:23 +0300

    Task Done

---
+++ flag.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+def flag():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('N', type=int, help='input parameter N')
+
+    n = parser.parse_args().N
+    if not n % 2 == 0:
+        raise argparse.ArgumentError
+
+    Flag = '#'*(3*n+2)+'\n'+2*('#'+' '*3*n+'#'+'\n')
+    for i in range(int(n/2), 0, -1):
+        Flag += '#' + ' '*(n+i-1) + '*' + 'o'*2*(int(n/2) - i) + '*' + ' '*(n+i-1) + '#' + '\n'
+
+    for i in range(1, int(n/2)+1):
+        Flag += '#' + ' '*(n+i-1) + '*' + 'o'*2*(int(n/2) - i) + '*' + ' '*(n+i-1) + '#' + '\n'
+
+    Flag += 2*('#'+' '*3*n+'#'+'\n')+'#'*(3*n+2)+'\n'
+    print(Flag)
+
+
+if __name__ == "__main__":
+    flag()
